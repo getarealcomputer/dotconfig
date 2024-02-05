4c4375c588ec75ff1174e75adf3f91ba5b3661ad
getarealcomputer <getarealcomputer@gmail.com>
2024-02-05 12:24:40 +0700

    Updating Package Installer Script 	new file:   package-installer.sh

---
+++ package-installer.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+sudo dnf install mesa-* \
+  xcb-* \
+  libxcb* \
+  hwdata* \
+  cairo* \
+  libliftoff* \
+  libseat* \
+  libudev* \
+  pixman* \
+  libxkbcommon* \
+  libdrm* \
+  libpkgconf* \
+  libinput* \
+  libdisplay-info* \
+  glslang* \
+  vulkan* \
+  wayland* \
+  xwayland* \
+  xorg-x11-server-Xwayland* \
+  bemenu \
+  foot\
+  meson \
+  ninja* \
+  cmake \
+  make \
+  autoconf \
+  automake \
+  fd-find \
+  ripgrep \
+  -y
