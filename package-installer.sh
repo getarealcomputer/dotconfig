b77069349a53b9ba5d3ee3e90c95d5f6836d8f14
getarealcomputer <getarealcomputer@gmail.com>
2024-10-10 09:52:49 +0700

    Testing OS check

--- package-installer.sh
+++ package-installer.sh
@@ -1,32 +1,33 @@
 #! /bin/bash
+cat /etc/os-release | grep ^NAME= | sed -r 's/.*NAME="([a-zA-Z]+)".*/\1/'
 
-sudo dnf install mesa-* \
-  xcb-* \
-  libxcb* \
-  hwdata* \
-  cairo* \
-  libliftoff* \
-  libseat* \
-  libudev* \
-  pixman* \
-  libxkbcommon* \
-  libdrm* \
-  libpkgconf* \
-  libinput* \
-  libdisplay-info* \
-  glslang* \
-  vulkan* \
-  wayland* \
-  xwayland* \
-  xorg-x11-server-Xwayland* \
-  bemenu \
-  foot\
-  meson \
-  ninja* \
-  cmake \
-  make \
-  autoconf \
-  automake \
-  fd-find \
-  ripgrep \
-  -y
+#sudo dnf install mesa-* \
+#  xcb-* \
+#  libxcb* \
+#  hwdata* \
+#  cairo* \
+#  libliftoff* \
+#  libseat* \
+#  libudev* \
+#  pixman* \
+#  libxkbcommon* \
+#  libdrm* \
+#  libpkgconf* \
+#  libinput* \
+#  libdisplay-info* \
+#  glslang* \
+#  vulkan* \
+#  wayland* \
+#  xwayland* \
+#  xorg-x11-server-Xwayland* \
+#  bemenu \
+#  foot\
+#  meson \
+#  ninja* \
+#  cmake \
+#  make \
+#  autoconf \
+#  automake \
+#  fd-find \
+#  ripgrep \
+#  -y
